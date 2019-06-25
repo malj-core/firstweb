54414fea029c4093b6b4a35b7782e10fa4f06120
mlj <1337528922@qq.com>
2019-06-25 09:50:02 +0800

    1

---
+++ FirstWeb/FirstWeb.Web/Infrastructure/Config.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace FirstWeb.Web.Infrastructure
+{
+    public class Config
+    {
+        /// <summary>
+        /// 获取配置项目value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private static string GetAppSettingsValue(string key)
+        {
+            try
+            {
+                return ConfigurationManager.AppSettings[key];
+            }
+            catch (Exception e)
+            {
+                Log.Debug("获取配置项目value：" + e);
+            }
+            return null;
+        }
+        /// <summary>
+        /// 微信appId
+        /// </summary>
+        public static string appId => GetAppSettingsValue("appId");
+
+        /// <summary>
+        /// 微信appSecret
+        /// </summary>
+        public static string appSecret => GetAppSettingsValue("appSecret");
+    }
+}
