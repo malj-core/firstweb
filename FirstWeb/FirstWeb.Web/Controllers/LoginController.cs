73edf8c73e439041e40ccadc4fde950a7b63b788
mlj <1337528922@qq.com>
2019-06-26 15:56:42 +0800

    1

--- FirstWeb/FirstWeb.Web/Controllers/LoginController.cs
+++ FirstWeb/FirstWeb.Web/Controllers/LoginController.cs
@@ -1,9 +1,73 @@
-﻿using System.Web.Http;
+﻿using FirstWeb.Web.Infrastructure;
+using System;
+using System.Drawing;
+using System.Web.Mvc;
 
 namespace FirstWeb.Web.Controllers
 {
-    public class LoginController : ApiController
+    public class LoginController : Controller
     {
-        
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public void GetString()
+        {
+            var validateNum = ConverUtils.CreateRandomNum(4);
+            CreateImage(validateNum);
+            Session["ValidateNum"] = validateNum;
+        }
+
+        private void CreateImage(string validateNum)
+        {
+            if (validateNum == null || validateNum.Trim() == string.Empty)
+                return;
+            //生成BitMap图像
+            var image = new Bitmap(validateNum.Length * 12 + 12, 22);
+            var g = Graphics.FromImage(image);
+            try
+            {
+                //生成随机生成器
+                var random = new Random();
+                //清空图片背景
+                g.Clear(Color.White);
+                //画图片的背景噪音线
+                for (var i = 0; i < 25; i++)
+                {
+                    var x1 = random.Next(image.Width);
+                    var x2 = random.Next(image.Width);
+                    var y1 = random.Next(image.Height);
+                    var y2 = random.Next(image.Height);
+                    g.DrawLine(new Pen(Color.Silver), x1, x2, y1, y2);
+                }
+                var font = new Font("Arial", 12, (FontStyle.Bold | FontStyle.Italic));
+                var brush = new System.Drawing.Drawing2D.LinearGradientBrush(new Rectangle(0, 0, image.Width, image.Height), Color.Blue, Color.DarkRed, 1.2f, true);
+                g.DrawString(validateNum, font, brush, 2, 2);
+                //画图片的前景噪音点
+                for (int i = 0; i < 100; i++)
+                {
+                    int x = random.Next(image.Width);
+                    int y = random.Next(image.Height);
+                    image.SetPixel(x, y, Color.FromArgb(random.Next()));
+
+                }
+                //画图片的边框线
+                g.DrawRectangle(new Pen(Color.Silver), 0, 0, image.Width - 1, image.Height - 1);
+                var ms = new System.IO.MemoryStream();
+                //将图像保存到指定流
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+                Response.ClearContent();
+                Response.ContentType = "image/Gif";
+                Response.BinaryWrite(ms.ToArray());
+            }
+            finally
+            {
+                g.Dispose();
+                image.Dispose();
+            }
+        }
     }
 }
