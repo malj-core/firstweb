b81e53146e30aeb28c512011f99528f7b17e9bc7
mlj <1337528922@qq.com>
2019-06-26 14:35:17 +0800

    1.微信授权登录通用接口

---
+++ FirstWeb/WeChat.WebApi/Utils/HttpHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace WeChat.WebApi.Utils
+{
+    public class HttpHelper
+    {
+        public static string GetResponse(string url)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = httpClient.GetAsync(url).Result;
+            return response.IsSuccessStatusCode ? response.Content.ReadAsStringAsync().Result : null;
+        }
+
+        public static T GetResponse<T>(string url) where T : class, new()
+        {
+            var responseResult = GetResponse(url);
+            return responseResult != null ? JsonConvert.DeserializeObject<T>(responseResult) : null;
+        }
+    }
+}
---
+++ FirstWeb/WeChat.WebApi/WeChatRepository.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using WeChat.WebApi.Entities;
+using WeChat.WebApi.Utils;
+
+namespace WeChat.WebApi
+{
+    public class WeChatRepository
+    {
+        /// <summary>
+        /// 获取access_token、openId
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="appId"></param>
+        /// <param name="appSecret"></param>
+        /// <returns></returns>
+        public static AccessToken GetToken(string code, string appId, string appSecret)
+        {
+            var url =
+                $"https://api.weixin.qq.com/sns/oauth2/access_token?appid={appId}&secret={appSecret}&code={code}&grant_type=authorization_code";
+
+            var str = HttpHelper.GetResponse<AccessToken>(url);
+            return str;
+        }
+        /// <summary>
+        /// 获取微信用户信息
+        /// </summary>
+        /// <param name="openId"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static WechatUserInfo GetUserInfo(string openId, string token)
+        {
+            var url = $"https://api.weixin.qq.com/sns/userinfo?access_token={token}&openid={openId}&lang=zh_CN";
+
+            var result = HttpHelper.GetResponse(url);
+
+            if (string.IsNullOrEmpty(result))
+                return null;
+
+            var info = JsonConvert.DeserializeObject<WechatUserInfo>(result);
+
+            return info;
+        }
+    }
+}
